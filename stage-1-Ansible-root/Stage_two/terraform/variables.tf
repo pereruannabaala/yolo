05c04465ad799bfdf908345982d87914820e5e7e
pereruannabaala <pereruannabaala@gmail.com>
2025-10-26 14:03:31 +0300

    create variables files

--- stage-1-Ansible-root/Stage_two/terraform/variables.tf
+++ stage-1-Ansible-root/Stage_two/terraform/variables.tf
@@ -1,7 +1,23 @@
-variable "ansible_inventory" {
-  default = "../ansible/inventory.ini"
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
 }
 
-variable "ansible_playbook" {
-  default = "../ansible/playbook.yaml"
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
+variable "instance_ami" {
+  description = "AMI ID for the EC2 instances"
+  type        = string
+  default     = "ami-0cd59ecaf368e5ccf" # Amazon Linux 2 example
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
