b270585d285430844cb9c534f55f746a877acf7d
pereruannabaala <pereruannabaala@gmail.com>
2025-10-26 14:04:43 +0300

    add main.tf

--- stage-1-Ansible-root/Stage_two/terraform/main.tf
+++ stage-1-Ansible-root/Stage_two/terraform/main.tf
@@ -1,24 +1,77 @@
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
-    }
+resource "aws_security_group" "server_sg" {
+  name        = "server_sg"
+  description = "Security group for the EC2 instances"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow ICMP"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "server_sg"
   }
 }
 
-provider "null" {}
+resource "aws_instance" "server1" {
+  ami             = var.instance_ami
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.server_sg.name]
 
-resource "null_resource" "vagrant_up" {
-  provisioner "local-exec" {
-    command = "vagrant up"
+  tags = {
+    Name = "server1"
   }
 }
 
-resource "null_resource" "ansible_provision" {
-  depends_on = [null_resource.vagrant_up]
+resource "aws_instance" "server2" {
+  ami             = var.instance_ami
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.server_sg.name]
 
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/playbook.yaml --become --ask-become-pass"
+  tags = {
+    Name = "server2"
   }
+}
+
+output "server1_public_ip" {
+  description = "Public IP of the first EC2 instance"
+  value       = aws_instance.server1.public_ip
+}
+
+output "server2_public_ip" {
+  description = "Public IP of the second EC2 instance"
+  value       = aws_instance.server2.public_ip
 }
